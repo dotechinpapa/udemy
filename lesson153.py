ba0bc2d8ce1bb5d41ff6b69ca8b1a629baee184b
dotechinpapa <nagaitakamasa@nagaitakamasanoMacBook-Pro.local>
2022-01-09 14:14:50 +0900

    153の更新

--- lesson153.py
+++ lesson153.py
@@ -11,6 +11,7 @@ payload = {'key1': 'value1', 'key2': 'value2'}
 # with urllib.request.urlopen(url) as f:
 #     r = json.loads(f.read().decode('utf-8'))
 #     print(type(r))
+# aaa
 
 payload = json.dumps(payload).encode('utf-8')
 req = urllib.request.Request('http://httpbin.org/post', data=payload, method='POST')
